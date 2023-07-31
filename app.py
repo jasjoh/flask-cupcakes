9734215a1ff55637cd0678f2a1bf025c057594bb
Coco-ha <135057744+Coco-ha@users.noreply.github.com>
2023-07-31 15:58:05 -0500

    co

--- app.py
+++ app.py
@@ -12,9 +12,41 @@ connect_db(app)
 
 @app.get("/api/cupcakes")
 def get_all_cupcakes():
-    """ Return JSON representing all cupcakes. """
+    """ Get data of all cupcakes. """
 
     cupcakes = Cupcake.query.all()
     serialized = [cupcake.serialize() for cupcake in cupcakes]
 
     return jsonify(cupcakes=serialized)
+
+@app.get("/api/cupcakes/<int:cupcake_id>")
+def get_cupcake(cupcake_id):
+    """Get a data of single cupcake"""
+    
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+@app.post("/api/cupcakes")
+def create_cupcake():
+    """Create a cupcake with image_url, size, rating, flavor"""
+
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image_url = request.json.get("image_url")
+    flavor = request.json["flavor"]
+
+    new_cupcake = Cupcake(
+        flavor=flavor,
+        size=size,
+        image_url=image_url,
+        rating=rating
+        )
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
+
